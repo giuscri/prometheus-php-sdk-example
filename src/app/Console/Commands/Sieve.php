e0ad56fe18120ddd57273298ed03f0376c288549
Giuseppe Crinò <giuscri@gmail.com>
2026-03-13 17:20:24 +0100

    add comments

--- src/app/Console/Commands/Sieve.php
+++ src/app/Console/Commands/Sieve.php
@@ -21,17 +21,21 @@ class Sieve extends Command
 
         $this->info('This is generating a prime number...');
 
+        // 1. computing the actual value to add to counter
         $start = microtime(true);
         usleep(random_int(100000, 500000));
         $duration = microtime(true) - $start;
 
         $this->info("Prime found: $prime. Shutting down!");
 
+        // 2. connecting to collector
         $transport = (new OtlpHttpTransportFactory())->create(
-            'http://otel-collector:4318/v1/metrics',
+            'http://otel-collector:4318/v1/metrics', // use `http://datadog-agent-agent.datadog.svc.cluster.local:4318/v1/metrics`
             'application/json',
         );
         $reader = new ExportingReader(new MetricExporter($transport));
+
+        // 3. create metric
         $meterProvider = MeterProvider::builder()
             ->addReader($reader)
             ->setResource(ResourceInfo::create(Attributes::create([
@@ -41,15 +45,17 @@ class Sieve extends Command
                 'anything_can_be' => 'here'
             ])))
             ->build();
-
         $meter = $meterProvider->getMeter('custom_prefix');
         $counter = $meter->createCounter(
             'prime_sieving_duration_seconds',
             's',
             'Total time spent sieving primes',
         );
-        $counter->add($duration, ['head_tail' => random_int(0, 1) === 1 ? 'true' : 'false']);
 
+        // 4. set metric values + labels
+        $counter->add($duration);
+
+        // 5. flush to connector before exit
         $meterProvider->shutdown();
     }
 }
